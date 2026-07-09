0b1f567f223bfd7ac682536bd5bb18c7afb50023
l0nik <l0nik@mail.ru>
2026-07-09 19:03:10 +0300

    валидация Category

--- ewm-service/src/main/java/ru/practicum/ewm/category/dto/CategoryDtoPatch.java
+++ ewm-service/src/main/java/ru/practicum/ewm/category/dto/CategoryDtoPatch.java
@@ -1,10 +1,12 @@
 package ru.practicum.ewm.category.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CategoryDtoPatch {
     @NotBlank(message = "Не заполнено название категории")
+    @Size(max = 50)
     private String name;
 }
